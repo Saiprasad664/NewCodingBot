ea090d05112993b326945f76cefeba20b245ed69
Saiprasad664 <saiprasadvelpula05@gmail.com>
2024-06-21 13:04:16 +0530

    Add files via upload

---
+++ Helloworld.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Helloworld({firstName,LastName,email}) {
+    return(
+        <>
+        <h1>first Name:{firstName}</h1>
+        <h1>Last Name:{LastName}</h1>
+        <h1>email:{email}</h1>
+        </>
+    );
+    
+}
